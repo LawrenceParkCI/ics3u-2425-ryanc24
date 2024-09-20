51c9d33ad32aec182aded1454f8d4ad63029b295
342788239 <342788239@MXL1502T8P.tdsb.on.ca>
2024-09-20 11:11:07 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-RyanC/src/unit1/HelloWorld.java
@@ -0,0 +1,18 @@
+package unit1;
+
+/**
+* Descs project prints "Hello World" and a second statement
+* Date: Sept 19th, 2024
+* Author Caden Ryan
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This prints hello to the system console
+	}
+    System.out.println("Hello World");
+    System.out.println("Squid");
+    
+	}
+/*
+* this is a block comment.
