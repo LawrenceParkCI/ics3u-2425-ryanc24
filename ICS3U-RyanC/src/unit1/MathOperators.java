b038fa877fad70000c3408d5568cc0d38a8b5270
342788239 <342788239@MXL1502T8P.tdsb.on.ca>
2024-10-01 10:02:48 -0400

    math operations

---
+++ ICS3U-RyanC/src/unit1/MathOperators.java
@@ -0,0 +1,72 @@
+package unit1;
+/**
+ * Date: Oct. 1, 2024
+ * Description: Math with java
+ * @author Caden Ryan
+*/
+public class MathOperators {
+/**
+ *
+ * @param
+ */
+   public static void main (String [] args) {
+      /*
+         Pre: Kind of Math, but not really
+         What do you notice is happening?
+      */ 
+	   //the system is adding to words to together to create a compound word
+      
+      //      System.out.println("Butter" + "fly");
+      
+      //      System.out.println("1 + 2 + 3 + 4 + 5");
+      
+      //      System.out.println(1 + 2 + 3 + 4 + 5);
+      
+      //      System.out.println(1 + 2 + "3 + 4 + 5");
+      
+      //      System.out.println("1 + 2 + 3" + 4 + 5);
+      
+      
+      //Why do you think the last two outputs act so differently?
+      
+      
+      //Summary: What are two possible roles of the + operator?
+     // Addition and adding qoutes
+      
+      /*
+         Part 1 
+         Figure out what is the meaning of each operator. Use print statements
+         to verify your answer and explain.
+      */
+         
+      // + means: Addition
+      
+      // - means: SUbtraction
+      
+      // * means: Multiplication
+      
+      // / means: Division
+      
+      
+      
+      //Print the following expression: (3 + 2) * 5
+      //Print the following expression: 3 + 2 * 5
+      
+      //Was this expected? Why or why not?
+      /* Yes because the brackets are irrelavant
+         Part 2 - Calculate Using Operators
+         Print out, using one println statement, what 45 degrees Celsius is in Fahrenheit
+         System.out.println("45c * 9/5 +32")
+         Print out, using a print and a println statement, what 900 Fahrenheit is in Celsius
+      */     System.out.println("900f-32*5/9");
+      
+      /*
+         Part 3
+         Figure out what is the meaning of % as a math operator. Use print statements
+         to check your answer.
+      */ System.out.println("24%4");
+      
+      //I think % means: 
+      //the remaing value after a division question
+   }
+}
