c2067251d0865c2967b56d4e59970142e704d3cd
342788239 <342788239@MXL1502T8P.tdsb.on.ca>
2024-10-02 11:15:24 -0400

    using string variables

---
+++ ICS3U-RyanC/src/unit1/MakeLabels.java
@@ -0,0 +1,31 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class MakeLabels {
+
+	public static void main(String[] args) {
+			
+		Scanner sc = new Scanner(System.in);
+		
+		String subject;
+		String name;
+		
+		System.out.println("Type in the subject");
+		subject=sc.nextLine();
+		System.out.println("Type in the name");
+		name=sc.nextLine();
+		
+		System.out.println();
+		System.out.println("***************************");
+		System.out.println(name);
+		System.out.println(subject);
+		System.out.println("***************************");
+		sc.close();
+	
+	
+	
+	
+	}
+
+}
