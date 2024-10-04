83a8fa3a6bdf49fce8e0f010ebd23c9951294c47
342788239 <342788239@MXL1502T8P.tdsb.on.ca>
2024-10-04 10:39:36 -0400

    w

---
+++ ICS3U-RyanC/src/unit1/InputFP.java
@@ -0,0 +1,29 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class InputFP {
+   
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		double num1, num2;
+		System.out.println("enter your first number");
+		num1 = sc.nextDouble();
+		
+		System.out.println("enter your second number");
+		num2 = sc.nextDouble();
+		
+		System.out.println();
+		
+		System.out.println("The first number entered was" + num1);
+		System.out.println("The second number entered was" + num2);
+		
+		
+		
+		
+		
+		
+	}
+
+}
