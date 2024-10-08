a074bce1008ff9dc127106a375ba910868babab4
342788239 <342788239@MXL1502T8P.tdsb.on.ca>
2024-10-08 11:28:00 -0400

    Using variables to calculate the cost of a carpet per square meter

--- ICS3U-RyanC/src/unit1/Carpet.java
+++ ICS3U-RyanC/src/unit1/Carpet.java
@@ -1,10 +1,38 @@
 package unit1;
 
+import java.util.Scanner;
+
 public class Carpet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// declare variables
+		double length, width, cost;
+		double total;
+		
+		//receive user input
+		System.out.println("State the length of your futurecarpet (m)");
+		length = sc.nextDouble();
+		
+		System.out.println("State the width of your future carpet (m)");
+		width = sc.nextDouble();
+		
+		System.out.println("State the cost of your future carpet per sqm");
+		cost = sc.nextDouble();
+		
+		//print output
+		System.out.println("\nThe cost of your future carpet is: " + (total = cost*length*width) + "dollars.");
+		sc.close();
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
